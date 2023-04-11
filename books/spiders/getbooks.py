aac7fb89429bb7d3169e03d57286c685eb724391
Linda Ngo <ln.lindango@gmail.com>
2023-04-10 22:05:24 -0700

    Update getbooks.py

--- books/spiders/getbooks.py
+++ books/spiders/getbooks.py
@@ -1,38 +1,29 @@
 import scrapy
 from scrapy import Request
 
-
 class GetbooksSpider(scrapy.Spider):
     name = 'getbooks'
     allowed_domains = ['books.toscrape.com']
     start_urls = ['http://books.toscrape.com/']
 
     def parse(self, response):
-        for x in response.xpath('//article[@class="product_pod"]'):
-            title = x.xpath('//h3/a/@title').extract_first()
-            
-            price = x.xpath('//div[@class="product_price"]/p[@class="price_color"]/text()').extract_first()
-            
-            star = x.xpath('//p/@class').extract_first().split()[-1]
-            
-            rel_url = x.xpath('//h3/a/@href').extract_first()
+        for book in response.xpath('//article[@class="product_pod"]'):
+            rel_url = book.xpath('.//h3/a/@href').get()
             abs_url = response.urljoin(rel_url)
             
-            yield Request(abs_url, callback = self.parse_page, dont_filter = True, meta = {'Title': title, 'Price': price, 'Star': star, 'URL': abs_url})
-        
-        rel_next_url = response.xpath('//li[@class="next"]/a/@href').extract_first()
-        abs_next_url = response.urljoin(rel_next_url)
-        
-        yield Request(abs_next_url, callback = self.parse)
+            yield Request(abs_url, callback=self.parse_book, meta={'url': abs_url})
 
-    def parse_page(self, response):
-        title = response.meta['Title']
-        price = response.meta['Price']
-        star = response.meta['Star']
-        abs_url = response.meta['URL']
-        avail = response.xpath('//p[@class="instock availability"]/text()').extract()
-        desc = response.xpath('//div[@id="product_description"]/following-sibling::p/text()').extract_first().strip()
-        
-        yield {'Title': title, 'Price': price, 'Star': star, 'URL': abs_url, 'Availability': avail, 'Description': desc}
-       
-        
+        next_url = response.xpath('//li[@class="next"]/a/@href').get()
+        if next_url:
+            yield response.follow(next_url, callback=self.parse)
+
+    def parse_book(self, response):
+        url = response.meta['url']
+        title = response.xpath('//h1/text()').get().strip()
+        price = response.xpath('//p[@class="price_color"]/text()').get()
+        rating = response.xpath('//p[contains(@class, "star-rating")]/@class')
+        star = rating.get().split()[-1] if rating else None
+        desc = response.xpath('//div[@id="product_description"]/following-sibling::p/text()')
+        desc = desc.get().strip() if desc else 'No description available'
+
+        yield {'url': url, 'Title': title, 'Price': price, 'Star': star, 'Description': desc}
